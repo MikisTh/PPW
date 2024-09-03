69e441ebb1d382cc6238e253432c6651733d916f
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-03 19:30:03 -0300

    Add
    Add

---
+++ PPW2024.2/Objetos/ex.js
@@ -0,0 +1,10 @@
+for (let foods in listFoods) {
+    console.log(foods, listFoods[foods]);
+}
+
+//inserindo novo objeto e alterar um dos campos: 
+
+let food2 = {...foods, name: banana}
+
+let food3 = { food, name: maça}
+
