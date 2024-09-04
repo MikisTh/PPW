85495c6fadd460f558fce6e332214b86bc624214
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-03 23:38:32 -0300

    Atualizar o ex1.js

--- PPW2024.2/Objetos/ex1.js
+++ PPW2024.2/Objetos/ex1.js
@@ -1,32 +1,25 @@
-meuCarro["fabricacao"] = "Ford";
-meuCarro["modelo"] = "Mustang";
-meuCarro["ano"] = 1969;
+aluno ["nome"] = "nome do aluno";
+aluno["matricula"] = "000000;
+aluno["cpf"] = 000.000.000-00;
 
-function Carro(marca, modelo, ano) {
-  this.marca = marca;
-  this.modelo = modelo;
-  this.ano = ano;
+function Aluno (nome, matricula, cpf) {
+  this.nome = nome;
+  this.matricula = matricula;
+  this.cpf = cpf;
 }
 
-var meucarro = new Carro("Eagle", "Talon TSi", 1993);
+var aluno1 = new Aluno("Thiago", "000001",000.000.000-01);
 
-var carroDeKen = new Carro("Nissan", "300ZX", 1992);
-var carroDeVPG = new Carro("Mazda", "Miata", 1990);
 
-// Encapsulamento das propriedades e métodos de Animal
-var Animal = {
-  tipo: "Invertebrados", // Propriedades de valores padrão
+var Aluno = {
+  tipo: "veterano", 
   qualTipo: function () {
-    // Método que ira mostrar o tipo de Animal
     console.log(this.tipo);
   },
 };
 
-// Cria um novo tipo de animal chamado animal1
-var animal1 = Object.create(Animal);
-animal1.qualTipo(); // Saída:Invertebrados
-
-// Cria um novo tipo de animal chamado Peixes
-var peixe = Object.create(Animal);
-peixe.tipo = "Peixes";
-peixe.qualTipo(); // Saída: Peixes
+var aluno1 = Object.create(aluno);
+animal1.qualTipo();
+var aluno = Object.create(aluno);
+aluno.tipo = "bolsita";
+aluno.qualTipo(); 
