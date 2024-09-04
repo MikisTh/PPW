9f8f70322406b11e455c48be8adf48e15f240925
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-04 10:16:47 -0300

    Add
    Add

---
+++ PPW 2024.2/Objetos/ex.js
@@ -0,0 +1,17 @@
+for (let foods in listFoods) {
+    console.log(foods, listFoods[foods]);
+}
+
+//inserindo novo objeto e alterar um dos campos: 
+
+let food2 = {...foods, name: banana}
+
+let food3 = { food, name: maça}
+
+food4 = {} // objeto vazio
+
+let aluno = {matrícula, nome, cpf, idade}
+JSON.stringify(aluno)
+
+"mat =" +aluno.matrícula+",nome="+aluno.nome+",cpf:"+aluno.cpf+",idade="+aluno.idade+""
+'mat = ${aluno.matricula} ...'
